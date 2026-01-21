927ae07034de8767db7c3649343c786f7f3497f3
pisco <piscopiello.italo@gmail.com>
2026-01-21 15:20:55 +0100

    fix: disable autofill on search input

--- index.php
+++ index.php
@@ -288,7 +288,8 @@ $apiKey = $user['gemini_api_key'] ?? null;
                 <?php endif; ?>
 
                 <div class="relative w-full md:w-64">
-                    <input type="text" id="taskSearch" placeholder="Cerca task..." onkeyup="searchTasks()"
+                    <input type="text" id="taskSearch" name="search_tasks_unique" autocomplete="off"
+                        placeholder="Cerca task..." onkeyup="searchTasks()"
                         class="w-full bg-white border border-slate-200 rounded-full px-4 py-2 text-sm focus:ring-2 focus:ring-indigo-500 outline-none">
                 </div>
             </div>
