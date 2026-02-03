a52580b33bd418a2d2e08cda1b97c41d6c64d664
pisco <piscopiello.italo@gmail.com>
2026-02-03 11:07:46 +0100

    fix notifiche su mobile

--- index.php
+++ index.php
@@ -835,7 +835,7 @@ $isAnyTeamAdmin = $stmt->fetchColumn();
     </head>
     <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/toastify-js"></script>
     <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.min.css">
-    <script src="js/shared.js"></script>
+    <script src="js/shared.js?v=<?php echo time(); ?>"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.1/datepicker.min.js"></script>
     <script src="js/index_logic.js"></script>
 
